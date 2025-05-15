421080a6309f5ce8aeb925fa1ed6e093c8dd7592
Amy <a0900638749@gmail.com>
2025-05-15 13:01:24 +0800

    gaygay

--- src/main/java/frc/robot/Elevator.java
+++ src/main/java/frc/robot/Elevator.java
@@ -6,28 +6,26 @@ package frc.robot;
 
 import com.revrobotics.servohub.ServoHub.ResetMode;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-
-import edu.wpi.first.units.measure.Angle;
-import frc.robot.Tool.Levels;
 
 /** Add your docs here. */
 public class Elevator {
     private final SparkMax LeftElevatorMotor;
     private final SparkMax RightElevatorMotor;
+    private final Claw claw;
     private double height=0.0;
-    private double SafeAngle=25;
-    private Tool.Levels TheLevel;
+    private double SafeAngle;
 
     public Elevator(){
       LeftElevatorMotor = new SparkMax(5, MotorType.kBrushless);
       RightElevatorMotor = new SparkMax(6, MotorType.kBrushless);
-      LeftElevatorMotor.configure(Tool.SetElevatorConfig(true), com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-      RightElevatorMotor.configure(Tool.SetElevatorConfig(false), com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+      claw = new Claw();
+      LeftElevatorMotor.configure(Tool.SetConfig(true), com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+      RightElevatorMotor.configure(Tool.SetConfig(false), com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     }
-    public void SetPosition(){
+
+     public void SetPosition(){
       height=height/11.43*15;
       LeftElevatorMotor.getClosedLoopController().setReference(height, ControlType.kPosition);
       RightElevatorMotor.getClosedLoopController().setReference(height, ControlType.kPosition);
@@ -36,15 +34,16 @@ public class Elevator {
       return (LeftElevatorMotor.getEncoder().getPosition()+RightElevatorMotor.getEncoder().getPosition())/2.0;
     }
     public void SetPoint(Claw claw){
-      height=TheLevel.GetHeight();
       claw.SetAngle(SafeAngle);
       if(Tool.IsInRange(GetTheHeight(),TheLevel.GetHeight())==false){
+        height=TheLevel.GetHeight();
         SetPosition();
-        claw.SetAngle(TheLevel.GetAngle());
+        }
+      claw.SetAngle(TheLevel.GetAngle());
+    }   
+      public void SetLevel(Levels level){
+        this.TheLevel=level;
       }
-    }
-    public void SetLevel(Levels level){
-      this.TheLevel=level;
-    }
   }
-
+    
+    
