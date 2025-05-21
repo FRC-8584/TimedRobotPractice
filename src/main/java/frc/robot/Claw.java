21df5434bdbc0db0269d27258407ce689e044e7e
Issac <nier.a2b9s042153@gmail.com>
2025-05-21 12:45:16 +0800

    ttl

--- src/main/java/frc/robot/Claw.java
+++ src/main/java/frc/robot/Claw.java
@@ -2,7 +2,11 @@ package frc.robot;
 
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 
 public class Claw {
@@ -14,9 +18,11 @@ public class Claw {
     public Claw(){
         catch_motor = new SparkMax(9, MotorType.kBrushed);
         angle_motor = new SparkMax(8, MotorType.kBrushed);
-      
-        }
         
+        catch_motor.configure(SetClawConfig(false), ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        angle_motor.configure(SetClawConfig(true), ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        }
+
     public void Anglepower(){
         angle_motor.getClosedLoopController().setReference(angle, ControlType.kPosition);
             if(catch_power = true){
@@ -25,6 +31,13 @@ public class Claw {
             else if(catch_power = false){
             catch_motor.set(0.5);
         }
+    }
+    public static SparkMaxConfig SetClawConfig(boolean Clawinvert){
+        SparkMaxConfig theConfig;
+        theConfig = new SparkMaxConfig();
+        theConfig.inverted(Clawinvert).idleMode(IdleMode.kBrake);
+        theConfig.closedLoop.pid(0.1, 0, 0.6);
+        return theConfig;
     }
     public void SetAngle(double angleunit){
         angle = angleunit/10;
--- src/main/java/frc/robot/Elevator.java
+++ src/main/java/frc/robot/Elevator.java
@@ -6,12 +6,11 @@ import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 public class Elevator {
     private SparkMax left_motor;
     private SparkMax right_motor;
-    private SparkMaxConfig config1;
-    private SparkMaxConfig config2;
     public double current_position;
     public double round = 0.7;
     public double height=0.0;
@@ -19,13 +18,20 @@ public class Elevator {
     public Elevator(){
         left_motor = new SparkMax(5, MotorType.kBrushed);
         right_motor = new SparkMax(6, MotorType.kBrushed);
-        config1 = new SparkMaxConfig();
-        config2 = new SparkMaxConfig();
+        
 
-        left_motor.configure(config1,ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-        right_motor.configure(config2,ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        left_motor.configure(setElevatorConfig(false),ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        right_motor.configure(setElevatorConfig(true),ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
         
     }    
+    public static SparkMaxConfig setElevatorConfig(boolean setElevator){
+        SparkMaxConfig thisConfig;
+        thisConfig = new SparkMaxConfig();
+        thisConfig.inverted(setElevator).idleMode(IdleMode.kBrake);
+        thisConfig.closedLoop.pid(.1, 1e-4, 0.4).iZone(0.1);
+        return thisConfig;
+
+    }
     public void ElevatorPower(){
          left_motor.getClosedLoopController().setReference(height/round, ControlType.kPosition);
         right_motor.getClosedLoopController().setReference(height/round, ControlType.kPosition);
